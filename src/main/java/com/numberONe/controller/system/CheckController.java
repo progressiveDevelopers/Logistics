9ef08624c31bcb84a628f7464b323385bbcfed91
gaoguofeng <18516523981@163.com>
2018-01-18 17:03:40 +0800

    添加部门考核模块控制器

---
+++ src/main/java/com/numberONe/controller/system/CheckController.java
@@ -0,0 +1,37 @@
+/** 
+ * @ClassName: CheckController 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author gaoguofeng 
+ * @date 2018年1月18日 下午3:16:12  
+ */
+package com.numberONe.controller.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.numberONe.controller.index.BaseController;
+import com.numberONe.util.Common;
+
+/** 
+ * @ClassName: CheckController 
+ * @Description: 部门考核模块控制器 
+ * @author: gaoguofeng 
+ * @email: 18516523981@163.com
+ * @date: 2018年1月18日 下午3:24:02 
+ */
+
+@Controller
+@RequestMapping("/check/")
+public class CheckController extends BaseController{
+	
+	@RequestMapping("index")
+	public String welcome(){
+		return Common.BACKGROUND_PATH + "/system/check/index";
+	}
+	
+	@RequestMapping("checkResult")
+	public String assessList(){
+		return Common.BACKGROUND_PATH + "/system/check/checkResult";
+	}
+	
+}
